3849a8d4d160b12fb62146bd3f0242d7cfa48487
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-05-21 14:32:54 +0900

    Initial Commit

---
+++ myTableViewTest03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTableViewTest03
+//
+//  Created by D7703_24 on 2019. 5. 21..
+//  Copyright © 2019년 D7703_24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
